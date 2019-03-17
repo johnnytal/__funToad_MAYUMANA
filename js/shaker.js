87380bc1c15c231f546bbe87783a0471c5b12b19
johnnytal <johnnytal9@gmail.com>
2019-03-17 21:23:14 +0200

    shaker

--- js/shaker.js
+++ js/shaker.js
@@ -7,6 +7,7 @@ var shakerMain = function(game){
 	resetTouching = true;
 	
 	sensFactor = 0;
+	distanceFactor = 0;
 	
 	resetAccel = true;
 	
@@ -17,7 +18,9 @@ var shakerMain = function(game){
 shakerMain.prototype = {
     create: function(){
     	game.stage.backgroundColor = '#ffffff';
+    	
     	sensFactor = 0;
+    	distanceFactor = 0;
     	
     	bg = game.add.image(0, 0, 'bg');
     	bg.alpha = 0.6;
@@ -57,8 +60,33 @@ shakerMain.prototype = {
         	setTimeout(function(){minus.tint = 0xffffff;},100);
         }, this);
         
+        plusD = game.add.sprite(620, 100, 'plus');
+        plusD.scale.set(.85, .85);
+        plusD.alpha = 0.85;
+        plusD.inputEnabled = true;
+        plusD.events.onInputDown.add(function(){
+        	distanceFactor += 2;
+        	distanceText.text = "Distance\nfactor: " + roundIt(distanceFactor);
+        	plusD.tint = 0xf04030;
+        	setTimeout(function(){plusD.tint = 0xffffff;},100);
+        }, this);
+        
+        minusD = game.add.sprite(525, 100, 'minus');
+        minusD.scale.set(.85, .85);
+        minusD.alpha = 0.85;
+        minusD.inputEnabled = true;
+        minusD.events.onInputDown.add(function(){
+        	distanceFactor -= 2;
+        	distanceText.text = "Distance\nfactor: " + roundIt(distanceFactor);
+        	minusD.tint = 0xf04030;
+        	setTimeout(function(){minusD.tint = 0xffffff;},100);
+        }, this);
+        
         sensText = game.add.text(530, 230, "Sensitivity\nfactor: " + roundIt(sensFactor), 
         {font: '22px', fill: 'white'});
+        
+        distanceText = game.add.text(530, 30, "Distance\nfactor: " + roundIt(distanceFactor),
+        {font: '22px', fill: 'black'});
         
         try{navigator.accelerometer.watchAcceleration(readAccel, onError, { frequency: 1});} catch(e){}
     },
@@ -87,16 +115,16 @@ shakerMain.prototype = {
 function readAccel(acceleration){
 	accelX = acceleration.x;
     
-    circle.y = MIDDLE + (acceleration.x * (5.7 + sensFactor) - 2);
+    circle.y = MIDDLE + (accelX * (5.7 + sensFactor) - 2);
     
-    if ((accelX < 0 && lastAccelX < 0) || (accelX > 0 && lastAccelX > 0)){
+  /*  if ((accelX < 0 && lastAccelX < 0) || (accelX > 0 && lastAccelX > 0)){
     	resetAccel = false;
     }
     else{
     	resetAccel = true;
     }
     
-    lastAccelX = accelX;
+    lastAccelX = accelX;*/
 }
 
 function flash(_color){
